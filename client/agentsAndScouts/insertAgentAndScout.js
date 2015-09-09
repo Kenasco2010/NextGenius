817d8230982912a16a0ce7cc28a663194f16ff86
Owohlet <ike.owoh@meltwater.org>
2015-09-09 13:34:51 +0000

    update working

--- client/agentsAndScouts/insertAgentAndScout.js
+++ client/agentsAndScouts/insertAgentAndScout.js
@@ -3,10 +3,26 @@ AutoForm.hooks({
     // Called when any operation succeeds, where operation will be
     // "insert", "update", "submit", or the method name.
     onSuccess: function(operation, result, template) {
-    	console.log("done")
-    	// FlashMessages.sendSuccess('Nice, all done!');
-    	Router.go('/agent-profile/:_id');
+    	console.log("done");
+    	Router.go('agentProfile');
     }
+},
+updateAgent: {
+	onSuccess: function(operation, result, template) {
+		console.log("updated");
+		Router.go('agentProfile');
+		return sAlert.success('Nice, all done!', {timeout: '20000', onRouteClose: false, position: 'top-left'});
 
+	},
+	onError: function(operation, error, template) {
+		return sAlert.error(error);
+	}
 }
+});
+
+Template.updateAgentAndScoutDetail.helpers({
+	anAgent: function() {
+		var id = Meteor.userId();
+		return Agents.findOne({ "owner": id });
+	}
 });
