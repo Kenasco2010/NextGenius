8d6dc7b286550b1286c7b09c42ba8ed560d5c954
Kwame Asiedu <kwame.asiedu@meltwater.org>
2015-09-09 15:45:58 +0000

    Fixed Image issues

--- client/player/playerHelpers.js
+++ client/player/playerHelpers.js
@@ -1,12 +1,17 @@
-// Template.playerProfile.events({
-// 	'click #delete': function(){
-// 	console.log("Im deleting")
-// 	Players.remove(this._id);
-// 	},
-// 	'click #update': function(){
-// 		Players.update(this._id);
-// 	}
-// })
+Template.clubAndAcademyProfile.events({
+    'click .updatePlayerDetail': function(e,t){
+        //alert("helloo you ")
+        Session.set('editclub', this._id)
+        //console.log(this._id)
+    }
+})
+
+Template.clubAndAcademyProfile.events({
+    'click .deletePlayerAccount':function(){
+        Meteor.users.remove({_id:this._id});
+        Router.go('/')
+    }
+})
 
 
 
