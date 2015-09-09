7213f54345a7023012d2fae2b0c729c1ddbd634c
Kwame Asiedu <kwame.asiedu@meltwater.org>
2015-09-09 16:26:29 +0000

    Remove error in code

--- client/player/playerHelpers.js
+++ client/player/playerHelpers.js
@@ -1,19 +1,1 @@
-Template.clubAndAcademyProfile.events({
-    'click .updatePlayerDetail': function(e,t){
-        //alert("helloo you ")
-        Session.set('editclub', this._id)
-        //console.log(this._id)
-    }
-})
-
-Template.clubAndAcademyProfile.events({
-    'click .deletePlayerAccount':function(){
-        Meteor.users.remove({_id:this._id});
-        Router.go('/')
-    }
-})
-
-
-
-
 
