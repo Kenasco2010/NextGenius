f6a4bec1fdaedcbf7ac622984cf842ab625b1b26
Kwame Asiedu <kwame.asiedu@meltwater.org>
2015-09-10 10:08:01 +0000

    Add Editorconfig to project

--- client/player/playerhook.js
+++ client/player/playerhook.js
@@ -1,10 +1,4 @@
 AutoForm.hooks({
-	playerForm:{
-		onSuccess: function(operation, result, template){
-			console.log("Submit successful");
-			Router.go('playerProfile')
-		}
-	}, 
 	updatePlayerDetail:{
 		onSuccess: function(operation ,result, template){
 			Router.go('allPlayers');
