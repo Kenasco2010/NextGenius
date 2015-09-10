2879c6e084ba1ca7896ed4eb68cc061020dbadc7
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-09-10 18:01:26 +0000

    still working on s3 stuff

---
+++ client/uploadVideos/videoHelpers.js
@@ -0,0 +1,12 @@
+Template.uploadVideos.helpers({
+    "files": function(){
+        if (Session.get('fileExists')) {
+            return S3.collection.find();
+        };
+    },
+    'complete': function() {
+        if (this.status == 'complete') {
+            return true;
+        };
+    }
+})
