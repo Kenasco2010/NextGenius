25bf3edc8fa1922a717305ec18e088ebdb6c4070
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-09-13 20:01:01 +0000

    players and clubs favorite is working, yet to work on videos favorite

--- client/player/playerHelpers.js
+++ client/player/playerHelpers.js
@@ -62,3 +62,4 @@ function nextTab(elem) {
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
+
--- client/request.js
+++ client/request.js
@@ -1,8 +1,8 @@
-var currentUser = Meteor.user();
-
-Template.clubAndAcademyProfile.events({
-    'click [data-action=request]': function() {
-        //assumes context is a instance of a user
-        this.requestFriendship();
-    }
-});
+//var currentUser = Meteor.user();
+//
+//Template.clubAndAcademyProfile.events({
+//    'click [data-action=request]': function() {
+//        //assumes context is a instance of a user
+//        this.requestFriendship();
+//    }
+//});
