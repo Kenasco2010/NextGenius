3a1663737438e15916201382411e77a72da6c0b3
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-09-13 14:45:47 +0000

    switching to favourite feature

---
+++ client/request.js
@@ -0,0 +1,8 @@
+var currentUser = Meteor.user();
+
+Template.clubAndAcademyProfile.events({
+    'click [data-action=request]': function() {
+        //assumes context is a instance of a user
+        this.requestFriendship();
+    }
+});
